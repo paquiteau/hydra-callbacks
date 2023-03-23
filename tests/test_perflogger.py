3a7b15005cd7e9df2638636ff9b4f9e885a3ed0d
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-03-23 18:03:39 +0100

    feat(test): increase coverage of perflogger testing.

--- tests/test_perflogger.py
+++ tests/test_perflogger.py
@@ -1,17 +1,25 @@
 import pytest
+import logging
 
 from hydra_callbacks import PerfLogger
 from time import sleep
 
 
-def test_perflogger():
+@pytest.mark.parametrize("func", [print, logging.getLogger("test_perflogger")])
+def test_perflogger(func):
 
-    with PerfLogger(print, name="test1"):
+    with PerfLogger(func, name="test1"):
         sleep(1)
 
-    with PerfLogger(print, name="test2"):
+    with PerfLogger(func, name="test2"):
         sleep(1)
 
     recap_str = PerfLogger.recap()
 
     assert recap_str == "test1: 1.00s, test2: 1.00s, Total: 2.00s"
+
+
+def test_perflogger_raises():
+    with pytest.raises(ValueError):
+        with PerfLogger("Not Callable", name="test1"):
+            sleep(1)
